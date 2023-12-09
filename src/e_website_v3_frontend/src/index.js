5a912e2cab4bb89b2623018c561ec2009ffd30e0
capuzr <capuzr@gmail.com>
2023-12-09 14:53:05 -0400

    Initial commit.

---
+++ src/e_website_v3_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { e_website_v3_backend } from "../../declarations/e_website_v3_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await e_website_v3_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
